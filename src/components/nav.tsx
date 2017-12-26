45db97896652d591daf7c3736378e92e131cfe4e
Mike Stupich <mike.stupich@gmail.com>
2017-12-26 14:47:37 -0500

    Moved files, added store and connected router to root

---
+++ src/components/nav.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import { Route, Link } from 'react-router-dom';
+
+
+const nav = () => {
+
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,20 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import * as React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'react-router-redux'
+import store, { history } from './store'
+import App from './components/app'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+import './index.css'
+
+const target = document.querySelector('#root')
+render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <div>
+        <App />
+      </div>
+    </ConnectedRouter>
+  </Provider>,
+  target 
 );
-registerServiceWorker();
