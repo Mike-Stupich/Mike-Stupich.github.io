403cda6c74f21c5157ebd8701047e20c2bf8d1b0
Mike Stupich <mike.stupich@gmail.com>
2018-01-21 06:13:08 -0500

    Added fixed menu to Nav

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -3,49 +3,96 @@ import { Link, Route } from 'react-router-dom';
 import {
     Container,
     Menu,
-    Segment
+    Segment,
+    Visibility
 } from 'semantic-ui-react';
 
 interface INavProps {
     text: string;
 }
 
-export default class Nav extends React.Component<INavProps, any> {
+interface INavState {
+    visible: boolean;
+}
+
+export default class Nav extends React.Component<INavProps, INavState> {
+
     public render() {
         return (
-            <Segment
-                inverted={true}
-                textAlign='center'
-                style={{ minHeight: 50, padding: '1em 0em' }}
-                vertical={true}
+            <Visibility
+                onBottomPassed={this.showFixedMenu}
+                onBottomVisible={this.hideFixedMenu}
             >
-            <Container>
-                <Menu
+                <Segment
                     inverted={true}
-                    pointing={true}
-                    size='large'
+                    textAlign='center'
+                    style={{ minHeight: 50, padding: '1em 0em' }}
+                    vertical={true}
                 >
-                    <Menu.Item>{this.props.text}</Menu.Item>
-                    <Menu.Item as={Link} to='/'>
-                        <Menu.Item>Home</Menu.Item>
-                    </Menu.Item>
-                    <Menu.Item as={Link} to='/resume'>
-                        <Menu.Item>Resume</Menu.Item>
-                    </Menu.Item>
-                    <Menu.Item as={Link} to='/projects'>
-                        <Menu.Item>My Projects</Menu.Item>
-                    </Menu.Item>
-                    <Menu.Item as={Link} to='/aboutme'>
-                        <Menu.Item>About Me</Menu.Item>
-                    </Menu.Item>
-                    <Menu.Menu position='right'>
-                        <Menu.Item as='a' href='https://github.com/Mike-Stupich/React_Website'>
-                            <Menu.Item>Source Code</Menu.Item>
-                        </Menu.Item>
-                    </Menu.Menu>
-                </Menu>
-            </Container>
-        </Segment>
+                    <Container>
+                        <Menu
+                            inverted={true}
+                            pointing={true}
+                            size='large'
+                        >
+                            <Menu.Item>{this.props.text}</Menu.Item>
+                            <Menu.Item as={Link} to='/'>
+                                <Menu.Item>Home</Menu.Item>
+                            </Menu.Item>
+                            <Menu.Item as={Link} to='/resume'>
+                                <Menu.Item>Resume</Menu.Item>
+                            </Menu.Item>
+                            <Menu.Item as={Link} to='/projects'>
+                                <Menu.Item>My Projects</Menu.Item>
+                            </Menu.Item>
+                            <Menu.Item as={Link} to='/aboutme'>
+                                <Menu.Item>About Me</Menu.Item>
+                            </Menu.Item>
+                            <Menu.Menu position='right'>
+                                <Menu.Item as='a' href='https://github.com/Mike-Stupich/React_Website'>
+                                    <Menu.Item>Source Code</Menu.Item>
+                                </Menu.Item>
+                            </Menu.Menu>
+                        </Menu>
+                    </Container>
+                </Segment>
+            </Visibility>
         );
     }
+    private hideFixedMenu = () => {
+        this.setState({
+            visible: false
+        });
+    }
+    private showFixedMenu = () => {
+        this.setState({
+            visible: true
+        });
+    }
+    private FixedMenu = () => (
+        <Menu
+        fixed='top'
+        size='large'
+        >
+            <Menu.Item>{this.props.text}</Menu.Item>
+            <Menu.Item as={Link} to='/'>
+                <Menu.Item>Home</Menu.Item>
+            </Menu.Item>
+            <Menu.Item as={Link} to='/resume'>
+                <Menu.Item>Resume</Menu.Item>
+            </Menu.Item>
+            <Menu.Item as={Link} to='/projects'>
+                <Menu.Item>My Projects</Menu.Item>
+            </Menu.Item>
+            <Menu.Item as={Link} to='/aboutme'>
+                <Menu.Item>About Me</Menu.Item>
+            </Menu.Item>
+            <Menu.Menu position='right'>
+                <Menu.Item as='a' href='https://github.com/Mike-Stupich/React_Website'>
+                    <Menu.Item>Source Code</Menu.Item>
+                </Menu.Item>
+            </Menu.Menu>
+        </Menu>
+    )
+
 }
